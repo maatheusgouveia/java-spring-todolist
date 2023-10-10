69a680d145379dda3e1c6d82c41aadb5246fca30
Matheus Gouveia <maatheusgouveia@gmail.com>
2023-10-10 19:56:38 -0300

    create user controller

---
+++ src/main/java/br/com/matheusgouveia/todolist/user/UserModel.java
@@ -0,0 +1,10 @@
+package br.com.matheusgouveia.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    public String username;
+    public String name;
+    public String password;
+}
